11ec74d282a409aaba9805aca5f39b27c040094d
sheniaks <sheniaks@gmail.com>
2021-12-13 14:31:41 +0200

    fizzbuzz challenge

---
+++ Junior JavaScript Developer Interview Questions/fizzbizz.js
@@ -0,0 +1,15 @@
+function fb(n) {
+    for (let i=1; i<=n; i++){
+        if (i%3===0 && i%5===0) {
+        console.log('Fizzbuzz'); }
+        else if (i%3===0){
+        console.log('Fizz'); }
+        else if (i%5===0){
+        console.log('Buzz'); }
+        else {
+            console.log(i);
+        }   
+    }
+}
+
+fb(155);
